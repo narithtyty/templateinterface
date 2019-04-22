adb547855467bbd09c72ab80f188bb0ddb455cfa
Sovannarith Phan <32011081+narithtyty@users.noreply.github.com>
2019-04-22 17:12:50 +0700

    Add files via upload

---
+++ TemplateTimerService/Lib/Lib.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace TemplateTimerService.Lib
+{
+    class Lib
+    {
+        public static Boolean writeLOG(string msg)
+        {
+            string[] logfile = null;
+            Boolean result = true;
+            msg = "[" + DateTime.Now.ToString("yyyy-MM-dd HH mm ss") + "]: " + msg;
+            try
+            {
+                logfile = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "LOG*.txt");
+                if (logfile.Length == 0)
+                {
+                    using (StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LOG.txt", true))
+                    {
+                        sw.WriteLine(msg);
+                        sw.Close();
+                    }
+                }
+                else
+                {
+                    int lastfile = 0;
+                    string lastfilepath = "";
+                    for (int i = 0; i <= logfile.Length - 1; i++)
+                    {
+                        int numlog = 0;
+                        try
+                        {
+                            numlog = int.Parse(logfile[i].Split('\\')[logfile[i].Split('\\').Length - 1].Replace("SLIMSLOG", "").Replace(".txt", ""));
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+
+                        if (numlog > lastfile)
+                        {
+                            lastfile = numlog;
+                            lastfilepath = logfile[i];
+                        }
+                        else
+                        {
+                            lastfilepath = logfile[i];
+                        }
+                    }
+                    FileInfo mylogfile = new FileInfo(lastfilepath);
+                    long filesize = mylogfile.Length / 1000000;
+                    if (filesize > 4)
+                    {
+                        lastfilepath = lastfilepath.Replace(lastfilepath.Split('\\')[lastfilepath.Split('\\').Length - 1], "SLIMSLOG" + lastfile + 1 + ".txt");
+                    }
+                    using (StreamWriter sw = new StreamWriter(lastfilepath, true))
+                    {
+                        sw.WriteLine(msg);
+                        sw.Close();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
+        public static void backupFile(string pathfile, string backuppath)
+        {
+            try
+            {
+                string backpath = backuppath + "\\PROCESSED";
+                if (Directory.Exists(backpath) == false)
+                {
+                    Directory.CreateDirectory(backpath);
+                }
+                backpath += "\\" + System.DateTime.Now.ToString("yyyy");
+                if (Directory.Exists(backpath) == false)
+                {
+                    Directory.CreateDirectory(backpath);
+                }
+                backpath += "\\" + System.DateTime.Now.ToString("MM");
+                if (Directory.Exists(backpath) == false)
+                {
+                    Directory.CreateDirectory(backpath);
+                }
+                backpath += "\\" + System.DateTime.Now.ToString("dd");
+                if (Directory.Exists(backpath) == false)
+                {
+                    Directory.CreateDirectory(backpath);
+                }
+                backpath += "\\" + pathfile.Split(new string[] { "\\" }, StringSplitOptions.None)[pathfile.Split(new string[] { "\\" }, StringSplitOptions.None).Length - 1].Split(new string[] { "." }, StringSplitOptions.None)[0] + "-" + DateTime.Now.ToString("yyyy-MM-dd HH mm ss") + "." + pathfile.Split(new string[] { "\\" }, StringSplitOptions.None)[pathfile.Split(new string[] { "\\" }, StringSplitOptions.None).Length - 1].Split(new string[] { "." }, StringSplitOptions.None)[pathfile.Split(new string[] { "\\" }, StringSplitOptions.None)[pathfile.Split(new string[] { "\\" }, StringSplitOptions.None).Length - 1].Split(new string[] { "." }, StringSplitOptions.None).Length - 1];
+                File.Move(pathfile, backpath);
+            }
+            catch (Exception ex)
+            {
+                writeLOG("Backup File Error " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ TemplateTimerService/Service1.cs
@@ -0,0 +1,103 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateTimerService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Boolean chkprocess = true;
+        private static System.Timers.Timer myTimer = new System.Timers.Timer();
+        public static int scantime = 5000;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Lib.Lib.writeLOG("Service Start");
+            try
+            {
+                scantime = int.Parse(System.Configuration.ConfigurationSettings.AppSettings["scantime"].ToString()) * 1000;
+            }
+            catch (Exception ex)
+            {
+            }
+            Lib.Lib.writeLOG("Scan Time " + scantime);
+            myTimer.Interval = scantime;
+            myTimer.Elapsed += new System.Timers.ElapsedEventHandler(TimerEventProcessor);
+            myTimer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            Lib.Lib.writeLOG("Service Stop");
+        }
+        private void TimerEventProcessor(object myObject, EventArgs myEventArgs)
+        {
+            if (chkprocess == true)
+            {
+                chkprocess = false;
+                processMain();
+            }
+            else
+            {
+                myTimer.Stop();
+            }
+        }
+        private void processMain()
+        {
+            Lib.Lib.writeLOG("In process main");
+            string path = System.Configuration.ConfigurationSettings.AppSettings["Path"].ToString();
+            Lib.Lib.writeLOG("Path:" + path);
+            if (!string.IsNullOrEmpty(path))
+            {
+                string extension = System.Configuration.ConfigurationSettings.AppSettings["Filter"].ToString();
+                string subdire = System.Configuration.ConfigurationSettings.AppSettings["subfolder"].ToString();
+                string[] datafile = Directory.GetFiles(path, extension, subdire == "Y" ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+                Lib.Lib.writeLOG("file:" + datafile.Length);
+                for (int j = 0; j <= datafile.Length - 1; j++)
+                {
+                    Lib.Lib.writeLOG("filename:" + datafile[j].ToString());
+                    //string filecsv = toCSV(datafile[j].ToString(), path);
+                    // UploadSFTPFile(filecsv);
+                    // File.Delete(datafile[j].ToString());
+                    // File.Delete(filecsv);
+                }
+            }
+            else
+            {
+                Lib.Lib.writeLOG("Path Not Set in App.config");
+            }
+
+        }
+
+        private void UploadSFTPFile(string filename)
+        {
+            try
+            {
+                using (SftpClient client = new SftpClient(System.Configuration.ConfigurationSettings.AppSettings["server"].ToString(), int.Parse(System.Configuration.ConfigurationSettings.AppSettings["port"].ToString()), System.Configuration.ConfigurationSettings.AppSettings["user"].ToString(), System.Configuration.ConfigurationSettings.AppSettings["pass"].ToString()))
+                {
+                    client.Connect();
+                    client.ChangeDirectory(System.Configuration.ConfigurationSettings.AppSettings["folder"].ToString());
+                    using (FileStream fs = new FileStream(filename, FileMode.Open))
+                    {
+                        client.BufferSize = 4 * 1024;
+                        client.UploadFile(fs, Path.GetFileName(filename));
+                        Lib.Lib.writeLOG("Upload CSV Success " + filename);
+                    }
+                }
+                File.Delete(filename);
+            }
+            catch (Exception e)
+            {
+                // updateStatus(e.Message);
+            }
+        }
+    }
+}
